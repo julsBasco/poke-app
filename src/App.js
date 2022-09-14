17cc69a4e77aecb62786325817bfcc4dd4cf4053
Juls <jrlbasco@unionbankph.com>
2022-09-14 11:09:01 +0800

    Add API handlers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import  { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [page, setPage] = useState({start: 1, end: 10})
   const [pokemonList, setPokemonList] = useState([])
+  const [loading, setLoading] = useState(true)
 
   let baseURL = 'https://pokeapi.co/api/v2/'
 
@@ -21,17 +23,16 @@ function App() {
     let pokemonCollection = []
     let pokemonFormattedCollection = []
 
-    for (let i = 1; i <= 30; i++) {
+    for (let i = page.start; i <= page.end; i++) {
       let response = await getData(i)
       pokemonCollection.push(response)
     }
 
     pokemonFormattedCollection = pokemonCollection.map(item => {
       return(
-        <tr>
+        <tr key={item.name + item.id}>
           <td>
-            <img src= {item.sprites.front_default} />
-           
+            <img src= {item.sprites.front_default} alt={item.name} />
           </td>
           <td>
             {item.id}
@@ -43,30 +44,49 @@ function App() {
       )
     })
     setPokemonList(pokemonFormattedCollection)
+    setLoading(false)
   }
 
-
+  const nextPageHandler = () => {
+    const newPage = {
+      start: page.start + 10,
+      end: page.end + 10
+    }
+    setLoading(true)
+    setPage(newPage)
+  }
 
   useEffect(() => {
     genaratePokemonCollection()
-  }, [])
+  }, [page])
 
   return (
     <div>
-      <table>
-        <tr>
-          <th>
-            picture
-          </th>
-          <th>
-            id number
-          </th>
-          <th>
-            name
-          </th>
-        </tr>
-        {pokemonList}
-      </table>
+      {loading && <h1>loading . . . </h1>}
+      {!loading && 
+        <>
+          <button onClick={nextPageHandler}>next</button>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  picture
+                </th>
+                <th>
+                  id number
+                </th>
+                <th>
+                  name
+                </th>
+              </tr>
+            </thead>
+            
+            <tbody>
+              {pokemonList}
+            </tbody>
+          </table>
+        </>
+      }
     </div>
   );
 }
